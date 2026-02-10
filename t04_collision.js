6d11b74676b048e8d28a6eba10453e315933c850
23137gk <23137gk@hvhs.school.nz>
2026-02-10 15:13:19 +1300

    tuesday spell 5

--- t04_collision.js
+++ t04_collision.js
@@ -9,14 +9,21 @@
 /*******************************************************/
 function setup() {
 	console.log("setup: ");
-
+	cnv = new Canvas(windowWidth, windowHeight);
+	world.gravity.y = 10;
+	rectangle = new Sprite(950, 300, 90, 80, 'd');
+	rectangle.color = 'yellow';
+	rectangle.rotationSpeed = 2;
+	rectangle.vel.x = 2;
+	platform_1 = new Sprite(1300, 600, 500, 10, 'k');
+	platform_2 = new Sprite(700, 700, 500, 10, 'k');
 }
 	
 /*******************************************************/
 // draw()
 /*******************************************************/
 function draw() {
-	
+	background('white')
 }
 
 /*******************************************************/
