07a4b445bc6ae2effe5cccc61465c0d9ddd64e2e
23137gk <23137gk@hvhs.school.nz>
2026-02-13 12:29:32 +1300

    13th feb 2026

--- t01_create_sprite.js
+++ t01_create_sprite.js
@@ -9,6 +9,28 @@
 /*******************************************************/
 function setup() {
 	console.log("setup: ");
+	width = random(300,600);
+	height = random(300,600);
+	cnv = new Canvas (width, height);
+
+
+	red = new Sprite(50, 50, 100, 100);
+	red.color = 'red';
+
+	green = new Sprite(450, 50, 100, 100);
+	green.color = 'green';
+
+	yellow = new Sprite(50, 450, 100, 100);
+	yellow.color = 'yellow'
+
+	blue = new Sprite(450, 450, 100, 100);
+	blue.color = 'blue'
+
+	purple = new Sprite(250, 250, 100, 100);
+	purple.color = 'purple';
+
+}
+	/*
 	cnv = new Canvas(windowWidth, windowHeight);
 	rectangle = new Sprite(950, 400, 50, 80);
 	rectangle.color = 'pink';
@@ -21,7 +43,7 @@ function setup() {
 // draw()
 /*******************************************************/
 function draw() {
-	background('white');
+	background('turquoise');
 	
 }
 
